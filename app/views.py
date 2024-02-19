1997b645f92df6d1d3adf11213f912a5a8dcfd4b
Faruk Erdem <farukomererdem2003@gmail.com>
2024-02-19 22:07:41 +0300

    blind sqli

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from app.models import Product
 from django.db.models.functions import Extract
+from django.http import HttpResponseServerError
 # Create your views here.
 
 def cart(r):
@@ -19,29 +20,32 @@ def index(r):
     return render(r,'index.html')
 
 def shop(r):
-    order = r.GET.get('order')
-    order_by = r.GET.get("order_by")
-    if order_by is None:order_by="year"
-    if order is None:order='date'
-    if order=="date":
-        increasing=True
-        products = Product.objects.annotate(year_field=Extract("date",order_by))
-        lst_ = []
-        for i in products:
-            lst_.append({"product":i,"order_by_value":i.year_field})
-        lst_ =sorted(lst_,key=lambda d:d["order_by_value"],reverse=increasing)
-        i=0
-        lst__=[]
-        while i<len(products):lst__.append(lst_[i]["product"]);i+=1
-        products=lst__
-    elif order=="popularity":
-        products = Product.objects.order_by("popularity")
-        if order_by=="descending":
-            products=products.reverse()
-    else:
-        products=Product.objects.order_by("rating")
-        if order_by=="descending":
-            products=products.reverse()
-    return render(r,'shop.html',{
-        "products":products
-    })
+    try:
+        order = r.GET.get('order')
+        order_by = r.GET.get("order_by")
+        if order_by is None:order_by="year"
+        if order is None:order='date'
+        if order=="date":
+            increasing=True
+            products = Product.objects.annotate(year_field=Extract("date",order_by))
+            lst_ = []
+            for i in products:
+                lst_.append({"product":i,"order_by_value":i.year_field})
+            lst_ =sorted(lst_,key=lambda d:d["order_by_value"],reverse=increasing)
+            i=0
+            lst__=[]
+            while i<len(products):lst__.append(lst_[i]["product"]);i+=1
+            products=lst__
+        elif order=="popularity":
+            products = Product.objects.order_by("popularity")
+            if order_by=="descending":
+                products=products.reverse()
+        else:
+            products=Product.objects.order_by("rating")
+            if order_by=="descending":
+                products=products.reverse()
+        return render(r,'shop.html',{
+            "products":products
+        })
+    except Exception as e:
+        return redirect("shop")
